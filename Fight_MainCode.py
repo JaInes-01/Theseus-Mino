bc27a9ed2aaedfd9512047a19bd41ee1b88125d7
JaInes-01 <jade.huyghebaert1@hotmail.com>
2025-05-01 18:43:16 +0200

    Update Fight_MainCode.py

--- Fight_MainCode.py
+++ Fight_MainCode.py
@@ -1,18 +1,24 @@
 import pygame
+import time
 import math 
 from Objecten import VastObject, BewegendObject 
 from Speler import Speler 
-from Vijand import Vijand 
+from Vijand import Vijand, Minotaurus1, Minotaurus2
 from Map import Map
 SCREENWIDTH = 1000
 SCREENHEIGHT = 800
 
+background = pygame.image.load("background_fight.png")
+background = pygame.transform.scale(background, (SCREENWIDTH, SCREENHEIGHT))
+intro_background = pygame.image.load("intro_fight.png")
+intro_background = pygame.transform.scale(intro_background, (SCREENWIDTH, SCREENHEIGHT))
 pygame.init()
-screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT])
+screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 clock = pygame.time.Clock()
 fps = 30
 
 #de volgende code gebruiken om een zero matrix op te stellen
+
 #scherm opdelen in vierkantjes ('tiles') om obstakels, platforms en grond in te voegen
 aantal_blokken_horizontaal = 20
 tile_grootte = SCREENWIDTH/aantal_blokken_horizontaal
@@ -42,35 +48,7 @@ map_zero = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-map_back = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
-    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-]
 
-def teken_background(matrix):
-    for rij_index in range(len(matrix)):       #y-pos
-        for kol_index in range(len(matrix[rij_index])):
-            x = kol_index * tile_grootte
-            y = rij_index * tile_grootte
-            rect = pygame.Rect(kol_index * tile_grootte, rij_index * tile_grootte, tile_grootte, tile_grootte)
-            if matrix[rij_index][kol_index] == 0:
-                pygame.draw.rect(screen, (27, 75, 105), rect)
-            elif matrix[rij_index][kol_index] == 1:
-                pygame.draw.rect(screen, (0, 0, 0), rect)
   #matrix voor de map    
 map_niv2 = [[1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1],
 [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1],
@@ -98,80 +76,67 @@ map_niv1 = [[1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-    
-        
 
 
-
-
-class Bot(BewegendObject):
-    def __init__(self, x, y, snelheid, fact_basis, fact_hoogte, sprite_png):
-        super().__init__(x, y, fact_basis, fact_hoogte, sprite_png)
-        self.snelheid = snelheid
-        
-    def draw(self, screen):
-        screen.blit(self.sprite, (self.rect.x, self.rect.y))
-
-    def patrol(self):
-        if self.goal > self.x and self.goal == self.point_left:
-            self.goal = self.point_right
-            self.vx = self.snelheid
-        if self.goal<self.x and self.goal == self.point_right:
-            self.goal = self.point_left
-            self.vx = -self.snelheid
-        super().move(self.vx, self.vy)
-
-def rooster():
-    for line in range(0,aantal_blokken_horizontaal):
-        pygame.draw.line(screen, (255,255,255), (0, line*tile_grootte), (SCREENWIDTH, line*tile_grootte))
-        pygame.draw.line(screen, (255,255,255), (line*tile_grootte, 0), (line*tile_grootte, SCREENHEIGHT))
-
-
-list_of_objects=[]
-
-#niveau1 = Map(map_niv1)
-#ist_of_objects.append(niveau1)
-
+niveau1 = Map(map_niv1)
 niveau2 = Map(map_niv2)
-list_of_objects.append(niveau2)
-
 Theseus = Speler(20, 500, 5, 1/30, 1/30, "speler.png", niveau2)
-list_of_objects.append(Theseus)
+Mino1 = Minotaurus1(700, 550, 3, 1/10, 1/10, "Minotaurus.png", Theseus, niveau1)
+Mino2 = Minotaurus2(700, 550, 3, 1/10, 1/10, "Minotaurus.png", Theseus, niveau2)
 
-Minotaurus1 = Vijand(800, 550, 6, 1/10, 1/10, "Minotaurus.png")
-list_of_objects.append(Minotaurus1)
+#we maken een dictionary aan van alle levels, met hun map, objecten en vijand
+levels = {1:{"map": niveau1, "vijand" : Mino1}, 2:{"map": niveau2, "vijand": Mino2}}
 
-#Steen = Projectiel(800, 550, 3, 3, 1/15, 1/15, "Steen.png", Theseus.rect.x, Theseus.rect.y)
-#list_of_objects.append(Steen)
-
-running = True 
+def reset_level():
+    global Theseus, Mino1, Mino2
+    map_data = levels[huidige_level]["map"]
+    
+    Theseus = Speler(20, 500, 5, 1/30, 1/30, "speler.png", map_data)
+    if huidige_level == 1:
+        Mino1 = Minotaurus1(700, 400, 3, 1/9, 1/9, "Minotaurus.png", Theseus, niveau1)
+        levels[huidige_level]["vijand"] = Mino1
+        
+    elif huidige_level == 2:
+        Mino2 = Minotaurus2(700, 400, 3, 1/9, 1/9, "Minotaurus.png", Theseus, niveau2)
+        levels[huidige_level]["vijand"] = Mino2
+        
+        
+huidige_level = 1
+def game_run(levels):
+    global huidige_level
+    if huidige_level > len(levels):
+        return 
+    map_data = levels[huidige_level]["map"]
+    vijand = levels[huidige_level]["vijand"]  
+        
+    map_data.draw(screen)
+    Theseus.draw(screen)
+    Theseus.beweging(map_data)
+    vijand.draw(screen)
+    vijand.attack(Theseus)
+    vijand.beweging(Theseus, map_data)
+        
+        
+    if Theseus.hp < -10:
+        reset_level()
+    elif vijand.hp < -20:
+        huidige_level += 1
+        reset_level()
+        
+running = True
+reset_level()
 while running:
     clock.tick(30)
-    screen.fill((0, 0, 0))
+    screen.blit(background, (0,0))
+    game_run(levels)
+        
     
-    #rooster()
-    teken_background(map_back)
-    
-    niveau2.draw(screen)
-    
-    for object in list_of_objects:
-        if hasattr(object, "draw"):
-            object.draw(screen)
-        if hasattr(object,"patrol"):
-            object.patrol()
-        if hasattr(object, "beweging"):
-            object.beweging()
-        if hasattr(object, "volg_speler"):
-            object.volg_speler(Theseus)
-            
-        #if hasattr(object, "Gooien"):
-            #object.Gooien(Theseus)
     #ervoor zorgen dat speler niet uit het scherm komt
     if Theseus.rect.left < 0: 
         Theseus.rect.left = 0
