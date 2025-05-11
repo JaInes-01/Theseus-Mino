9debb5e7b3b5c5b84e2931609ffac1eb21581f90
TheaKM2006 <Theodora.kabanga.mukuna@vub.be>
2025-05-11 16:16:09 +0200

    Teleporteren_Doolhof.py

--- Teleporteren_Doolhof.py
+++ Teleporteren_Doolhof.py
@@ -6,6 +6,7 @@ kolommen = 33 #aantal kolommen van het doolhof (breedte)
 blokjesgrootte = 30 #de grootte van één vierkant in het doolhof
 
 def teleport_speler(speler, draad_locaties, draad_cooldowns):
+    # Bereken op welke rij en kolom de speler zich nu bevindt
     huidige_locatie = (speler.rect.y // blokjesgrootte, speler.rect.x // blokjesgrootte)
 
     # Controleer of de speler op een draad staat én of deze draad nog niet in cooldown zit
@@ -13,11 +14,15 @@ def teleport_speler(speler, draad_locaties, draad_cooldowns):
         # Kies een andere draad als bestemming
         mogelijke_bestemmingen = [loc for loc in draad_locaties if loc != huidige_locatie]
 
+        # Alleen teleporteren als er ten minste één andere draad beschikbaar is
         if mogelijke_bestemmingen:
+            # Kies willekeurig een van de andere draadlocaties als nieuwe positie
             nieuwe_locatie = random.choice(mogelijke_bestemmingen)
-            speler.rect.x = nieuwe_locatie[1] * blokjesgrootte
-            speler.rect.y = nieuwe_locatie[0] * blokjesgrootte
+            # Verplaats de speler naar de nieuwe locatie
+            speler.rect.x = nieuwe_locatie[1] * blokjesgrootte    # Zet x-positie (kolom)
+            speler.rect.y = nieuwe_locatie[0] * blokjesgrootte    # Zet y-positie (rij)
 
-            # Cooldowns instellen zodat speler niet meteen terug teleporteert
+            # Stel een cooldown in op zowel de huidige als de nieuwe draadlocatie,
+            # zodat de speler niet meteen terug kan teleporteren
             draad_cooldowns[huidige_locatie] = 100
             draad_cooldowns[nieuwe_locatie] = 100
