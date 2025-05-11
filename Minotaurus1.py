26e983c309bafd95a61487f4ff455fab4e7cff4f
JaInes-01 <jade.huyghebaert1@hotmail.com>
2025-05-11 16:22:20 +0200

    Update Minotaurus1.py

--- Minotaurus1.py
+++ Minotaurus1.py
@@ -1,8 +1,9 @@
 import pygame
 import time
-clock = pygame.time.Clock()
+import random
+from Objecten import VastObject, BewegendObject, SCREENWIDTH, SCREENHEIGHT, fps
 from Vijand import Vijand, SCREENWIDTH, SCREENHEIGHT, fps
-
+clock = pygame.time.Clock()
 
 class Minotaurus1(Vijand):
     def __init__(self, x, y, snelheid, basis, hoogte, sprite_png, speler, map_level):
@@ -108,9 +109,10 @@ class Minotaurus1(Vijand):
                 
         if self.damage_timer > 0:
             self.damage_timer -= self.no_damage_time_left/fps   # Decrease the timer
+            if self.damage_timer < 0:
+                self.damage_timer = 0   # Decrease the timer
             
         #voor de overgang naar de tweede level zal de minotaurus even verdoofd zijn
-        
         if self.health < 1:
             self.vertraag()
             
@@ -128,12 +130,31 @@ class Minotaurus1(Vijand):
             temp_sprite = pygame.transform.flip(temp_sprite, True, False)
         screen.blit(temp_sprite, self.rect.topleft)
         
-    def draw_healthbar(self, screen):
-        bar_width = self.basis  # even lang als de speler
+    def draw_healthbar(self, screen):# https://www.youtube.com/watch?v=E82_hdoe06M
+        bar_width = self.basis #even lang als de speler
         bar_height = 7
-        x = self.rect.x  # x positie gelijk aan de linkerpositie van de speler
-        y = self.rect.y - 10  # bar ligt een beetje boven de speler
-        pygame.draw.rect(screen, (255, 0, 0), (x, y, bar_width, bar_height))  # Rode balk = achtergrond van de healthbar
-        groene_breedte = bar_width * (self.health / self.max_health)  # Percentage maal bar width om te weten hoe groot de groene bar moet zijn
-        pygame.draw.rect(screen, (0, 255, 0), (x, y, groene_breedte, bar_height))  # Groene balk = huidige gezondheid
+        x = self.rect.x # x positie gelijk aan de linkerpositie van de speler
+        y = self.rect.y - 10 # bar ligt een beetje boven de speler
+        pygame.draw.rect(screen, (255, 0, 0), (x, y, bar_width, bar_height)) # =rode balk = achtergrond van de healthbar
+        groene_breedte = bar_width * (self.health / self.max_health) #geeft een percentage maal bar widht om te weten hoe groot de groen bar moet zijn
+        pygame.draw.rect(screen, (0, 255, 0), (x, y, groene_breedte, bar_height))
+        
+    
+    class Vallende_steen(BewegendObject):
+        def __init__(self, snelheid, fact_basis, fact_hoogte, sprite_png):
+            x = random.randint(0, SCREENWIDTH - 50)
+            y = 0
+            super().__init__(x, y,snelheid, fact_basis, fact_hoogte, sprite_png)
+            self.vy = snelheid
+            self.op_grond = False
+            self.Fz = 0.5
+            self.x = random.randint(0,SCREENWIDTH)
+            self.y = 0
+            self.basis = random.randint(10, 50) 
+            self.hoogte = self.basis
+            self.valt = False    
+            
+        def val(self):
+            self.vy += self.Fz
+            self.move(0,self.vy)
 
