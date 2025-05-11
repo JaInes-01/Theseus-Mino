190079dc1036fa3f47d5bff933819d496d2dd34c
JaInes-01 <jade.huyghebaert1@hotmail.com>
2025-05-11 18:39:05 +0200

    Update Minotaurus2.py

--- Minotaurus2.py
+++ Minotaurus2.py
@@ -1,9 +1,9 @@
 import pygame
 import time
-clock = pygame.time.Clock()
 from Vijand import Vijand, SCREENWIDTH, SCREENHEIGHT, fps
+clock = pygame.time.Clock()
 
-class MinotaurusVolg(Vijand):
+class Minotaurus2(Vijand):
     def __init__(self, x, y, snelheid, basis, hoogte, sprite_png, speler, map_level):
         super().__init__(x, y, snelheid, basis, hoogte, sprite_png, speler, map_level)
         self.facing_left = False #in het begin kijkt de minotaurus naar links want hij zit helemaal links
@@ -14,7 +14,6 @@ class MinotaurusVolg(Vijand):
         self.target = speler    
     
     def beweging(self, speler, map_level):
-        
         self.zwaartekracht()  
         self.vy = self.collisie_y(map_level)
         self.move(0, self.vy)
@@ -27,14 +26,15 @@ class MinotaurusVolg(Vijand):
             elif self.target.rect.centerx > self.rect.centerx and abs(self.target.rect.centerx - self.rect.centerx) > marge:
                 self.move(self.snelheid, 0)
                 self.facing_left = False
-             
-    
+                
     def draw(self,screen):
-    
         if self.facing_left:
             flipped_sprite = pygame.transform.flip(self.sprite, True, False)# gaat minautorus horizontaal flippen als het naar de een andere kant beweegt
             screen.blit(flipped_sprite, self.rect.topleft)
         else:
             screen.blit(self.sprite, self.rect.topleft)
+
+
+
 
 
