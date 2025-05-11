d425e503942365593b675f85d60b6c9a046cd630
JaInes-01 <jade.huyghebaert1@hotmail.com>
2025-05-11 18:34:39 +0200

    Update Fight_MainCode.py

--- Fight_MainCode.py
+++ Fight_MainCode.py
@@ -5,45 +5,32 @@ import math
 from Objecten import VastObject, BewegendObject, SCREENWIDTH, SCREENHEIGHT
 from Speler import Speler 
 from Vijand import Vijand
-from Minotaurus1 import Minotaurus1
-#from Minotaurus2 import Minotaurus2
-from Map import Map, aantal_blokken_horizontaal, tile_grootte, aantal_blokken_verticaal, map_niv1, map_niv2
+from Minotaurus1 import Minotaurus1, Vallende_steen, vallende_stenen, laatste_val, val_interval
+from Minotaurus2 import Minotaurus2
 from Mini_monsters import Mini_monsters
-from Vallende_steen import Vallende_steen, vallende_stenen, laatste_val, val_interval
-from MinotaurusVolg import MinotaurusVolg
-from Gewonnen import * 
-
-
+from Map import Map, aantal_blokken_horizontaal, tile_grootte, aantal_blokken_verticaal, map_niv1, map_niv2
+pygame.init()
+clock = pygame.time.Clock()
+fps = 30
 
 background = pygame.image.load("background_fight.png")
 background = pygame.transform.scale(background, (SCREENWIDTH, SCREENHEIGHT))
 intro_background = pygame.image.load("intro_fight.png")
 intro_background = pygame.transform.scale(intro_background, (SCREENWIDTH, SCREENHEIGHT))
-pygame.init()
-screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-clock = pygame.time.Clock()
-fps = 30
 
+screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 niveau1 = Map(map_niv1)
 niveau2 = Map(map_niv2)
-#niveau3 = Map(map_niv3)
-
-
-
-#LEVEL3
-#stenen = pygame.sprite.Group()
-#Mino2 = Minotaurus2(700, 550, 6 , 1/10, 1/10, "Minotaurus.png", Theseus, niveau1, stenen)
 
 #we maken een dictionary aan van alle levels, met hun map, objecten en vijand
 levels = {1:{"map": niveau1}, 2:{"map": niveau2}}
-
 huidige_level = 1
+start_time_level = None
 gevecht_gewonnen = False
 vijanden = []
 
-start_time_level = None
 def reset_level():
-    global vijanden, Theseus, Zwaard, MinoVolg
+    global vijanden, Theseus, Zwaard, Mino2
     map_data = levels[huidige_level]["map"]
     Zwaard = VastObject(tile_grootte, 7*tile_grootte, tile_grootte/SCREENWIDTH, tile_grootte/SCREENWIDTH, "zwaard2.png")
     if huidige_level == 1:
@@ -52,18 +39,15 @@ def reset_level():
         levels[huidige_level]["vijand"] = Mino1
     
     if huidige_level == 2:
-        global start_time_level, MinoVolg
+        global start_time_level, Mino2
         start_time_level = time.time()
         Theseus = Speler(20, 500, 5, 1/30, 1/30, "speler.png", map_data, Zwaard)
         Mini1 = Mini_monsters(4*tile_grootte, 2*tile_grootte, 2, tile_grootte/SCREENWIDTH, tile_grootte/SCREENWIDTH, "Monster.png", Theseus, niveau2)
         Mini2 = Mini_monsters(11*tile_grootte, 2*tile_grootte, 2, tile_grootte/SCREENWIDTH, tile_grootte/SCREENWIDTH, "Monster.png", Theseus, niveau2)
         Mini3 = Mini_monsters(17*tile_grootte, 6*tile_grootte, 2, tile_grootte/SCREENWIDTH, tile_grootte/SCREENWIDTH, "Monster.png", Theseus, niveau2)
-        MinoVolg= MinotaurusVolg(0, 500, 6, 1/10, 1/10, "Minotaurus.png", Theseus, niveau2)
+        Mino2 = Minotaurus2(0, 500, 6, 1/10, 1/10, "Minotaurus.png", Theseus, niveau2)
         levels[huidige_level]["vijand"] = [Mini1, Mini2, Mini3]
     
-    
-        
-
 def game_run(levels):
     global huidige_level, gevecht_gewonnen, laatste_val, start_time_level
     for event in pygame.event.get():
@@ -73,21 +57,22 @@ def game_run(levels):
         
     if huidige_level > len(levels):
         return 
+    
+    screen.blit(background, (0, 0))
     map_data = levels[huidige_level]["map"]
+    map_data.draw(screen)
     vijanden = levels[huidige_level]["vijand"]  
     pijl = VastObject(800, 500, 1/9, 1/9, "pijl_gevecht.png")
     
+    #aangezien level 2 meerdere vijanden heeft, maken we van alle vijanden een lijst
     if not isinstance(vijanden, list):
         vijanden = [vijanden]
-    screen.blit(background, (0, 0))
     for vijand in vijanden:
         vijand.beweging(Theseus, map_data, screen) 
         vijand.draw(screen)  
         vijand.attack(Theseus)
         vijand.draw_healthbar(screen)
-        
-    map_data.draw(screen)
-    
+                
     if huidige_level == 1:
         nu = time.time()
         if 0 < vijand.health < 4 and (Theseus.alive):
@@ -98,23 +83,24 @@ def game_run(levels):
             for steen in vallende_stenen:
                 steen.val()
                 steen.draw(screen)
+                #als de speler een steen op zijn hoofd krijgt, dan verliest hij hp
                 if steen.rect.colliderect(Theseus.rect) and Theseus.damage_timer == 0:
                     Theseus.health -= 1
                     Theseus.damage_timer = Theseus.no_damage_time_left
         
-        if all(vijand.health <= 0 for vijand in vijanden):  # Controleer of alle vijanden verslagen zijn
-            huidige_level += 1  # Ga naar level 2
-            reset_level()  # Reset de level instellingen
-            return  # Stop de huidige run en ga verder met de nieuwe level
+        if vijand.health < 1:  # Controleer of vijand verslagen is
+            pijl.draw(screen) #vokg de pijl om naar level 2 te gaan
+            if Theseus.rect.right == SCREENWIDTH: # Ga naar level 2
+                reset_level()  # Reset de level instellingen
+                return  # Stop de huidige run en ga verder met de nieuwe level
 
     if huidige_level == 2:
-        if time.time() - start_time_level > 5:
+        Zwaard.draw(screen) 
+        if time.time() - start_time_level > 5: #aangezien de minotaurus geneutraliseerd is in level 1 neemt het een beetje tijd voordat hij verschijnt
             MinoVolg.draw(screen)
             MinoVolg.beweging(Theseus, niveau2)
             if Theseus.rect.colliderect(MinoVolg.rect):
                 Theseus.alive = False
-            
-        Zwaard.draw(screen) 
         if Theseus.rect.colliderect(Zwaard.rect):
             Theseus.inventory.append(Zwaard)
             Zwaard.sprite.set_alpha(0)
@@ -122,24 +108,16 @@ def game_run(levels):
             Theseus.gewapend = True 
         else:
             Theseus.gewapend = False
-        
-        if all(vijand.health <= 0 for vijand in vijanden):  # Controleer of alle vijanden in level 2 verslagen zijn
+        if all(vijand.health <= 0 for vijand in vijanden):  # Controleer of alle vijanden verslagen zijn
             return gewonnen(True)
-                
+    #we tekenen Theseus op het einde zodat hij op de voorgrond verschijnt        
     Theseus.draw(screen)
     Theseus.beweging(map_data)
     Theseus.draw_healthbar(screen)
-    
     if Theseus.health <= 0:
         Theseus.alive = False 
-    
     if not Theseus.alive:
         return game_over(True)
-            
-    #if huidige_level == 3:
-        #als de speler level 3 wint dan wordt een flag ingevoerd die zegt dat hij het gevecht heeft gewonnen
-        #if vijand.health < 1 and Theseus.health > 0:
-            #gevecht_gewonnen = True 
     
 class Intro:
     def __init__(self):
