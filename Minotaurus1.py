1b00e83486e460b5f64882ebfedf7cec6ab881b9
JaInes-01 <jade.huyghebaert1@hotmail.com>
2025-05-11 16:29:01 +0200

    Update Minotaurus1.py

--- Minotaurus1.py
+++ Minotaurus1.py
@@ -140,21 +140,21 @@ class Minotaurus1(Vijand):
         pygame.draw.rect(screen, (0, 255, 0), (x, y, groene_breedte, bar_height))
         
     
-    class Vallende_steen(BewegendObject):
-        def __init__(self, snelheid, fact_basis, fact_hoogte, sprite_png):
-            x = random.randint(0, SCREENWIDTH - 50)
-            y = 0
-            super().__init__(x, y,snelheid, fact_basis, fact_hoogte, sprite_png)
-            self.vy = snelheid
-            self.op_grond = False
-            self.Fz = 0.5
-            self.x = random.randint(0,SCREENWIDTH)
-            self.y = 0
-            self.basis = random.randint(10, 50) 
-            self.hoogte = self.basis
-            self.valt = False    
+class Vallende_steen(BewegendObject):     
+    def __init__(self, snelheid, fact_basis, fact_hoogte, sprite_png):
+        x = random.randint(0, SCREENWIDTH - 50)
+        y = 0
+        super().__init__(x, y,snelheid, fact_basis, fact_hoogte, sprite_png)
+        self.vy = snelheid
+        self.op_grond = False
+        self.Fz = 0.5
+        self.x = random.randint(0,SCREENWIDTH)
+        self.y = 0
+        self.basis = random.randint(10, 50) 
+        self.hoogte = self.basis
+        self.valt = False    
             
-        def val(self):
-            self.vy += self.Fz
-            self.move(0,self.vy)
+    def val(self):
+        self.vy += self.Fz
+        self.move(0,self.vy)
 
